a607c98000f30ca17d5d8314f724501e1029194d
merofuruya <80584154+MeroFuruya@users.noreply.github.com>
2023-06-20 14:56:03 +0200

    update swagger def version to 1.0.1

--- src/openapi.ts
+++ src/openapi.ts
@@ -1,8 +1,8 @@
 import { JsonObject } from "swagger-ui-express"
 import { openapi as v0_0_2 } from "lmps-api-swagger-v0.0.2"
-import { openapi as v1_0_0 } from "lmps-api-swagger-v1.0.0"
+import { openapi as v1_0_1 } from "lmps-api-swagger-v1.0.1"
 
 export default {
   "v0.0.2": v0_0_2,
-  "v1.0.0": v1_0_0,
+  "v1.0.1": v1_0_1,
 } as { [version: string]: JsonObject }
